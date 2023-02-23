80b15b4d7b6223aae41debac01f9cd185c54d3cb
goldengrape <github@goldengrape.org>
2023-02-22 21:08:26 -0800

    init

---
+++ backend.py
@@ -0,0 +1,64 @@
+import pandas as pd
+import numpy as np
+
+def operand(type="POWP",    
+            param1=0,param2=0,param3=0.0,param4=0.0,
+            param5=0.0,param6=0.0,param7=0.0,param8=0.0,
+            target=0.0,weight=0.0,value=0.0):
+
+    # PMGT   3  14 0.000000000000E+00 0.000000000000E+00 0.000000000000E+00 0.000000000000E+00 0.000000000000E+00 0.000000000000E+00 -1.500000000000E+01 -1.000000000000E+00 -1.500000000000E+01
+
+    operand_str = f"{type}   {param1}  {param2} {param3} {param4} {param5} {param6} {param7} {param8} {target} {weight} {value}\n"
+    return operand_str
+def powp(surf, y, diameter, target,weight):
+    py=y/(diameter/2)
+    return operand(type="POWP", param1=surf, param6=py, target=target, weight=weight)
+
+def build_merit_function(
+        power_profile, 
+        lens_front_rowIndex,
+        lens_back_rowIndex,
+        
+        lens_diameter,
+        thickness_min, 
+        thickness_max, 
+        
+        if_1st_differential,
+        thickness_weight=1.0,
+        power_weight=1.0,
+        differential_weight=1.0
+        ):
+    ver_str = "VERS 190513\n"
+    thickness_str=operand(type="FTGT", 
+                    param1=lens_front_rowIndex,target=thickness_min,weight=thickness_weight)
+    thickness_str+=operand(type="FTLT",
+                    param1=lens_front_rowIndex,target=thickness_max,weight=thickness_weight)
+    p=power_profile.apply(lambda x: powp(lens_back_rowIndex,x[0],lens_diameter,x[1],power_weight), axis=1)
+    powp_str="".join(p)
+
+    d=power_profile.diff().dropna()
+    powp_start_index=3
+    diff_str=""
+    for i in range(len(d)):
+        op_No2=powp_start_index+i 
+        op_No1=op_No2+1
+        diff_str+=operand("DIFF", param1=op_No1, param2=op_No2,weight=0.0)
+    # divb_start_index=2+len(p)+len(d)+1
+    diff_start_index=2+len(p)+1
+
+    divb_str=""
+    for i in range(len(d)):
+        op_No=diff_start_index+i
+        divb_str+=operand("DIVB", param1=op_No, param3=d.iloc[i][0],target=d.iloc[i][1]/d.iloc[i][0],weight=differential_weight)
+
+    if if_1st_differential:
+        merit_str=ver_str+thickness_str+powp_str+diff_str+divb_str
+    else:
+        merit_str=ver_str+thickness_str+powp_str
+
+    return merit_str
+
+
+
+
+    pass 
---
+++ streamlit_app.py
@@ -0,0 +1,56 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from backend import build_merit_function
+from PIL import Image
+
+st.title('Zemax Merit Function Builder')
+col01,col02=st.columns(2)
+col01.markdown('''
+Generate a merit function to fit the lens Power Profile.
+A power profile file, in CSV file format, is required.
+
+* First Column: position
+* Second Column: power
+* No header
+''')
+col02.image(image=Image.open('demo.png'), caption="CSV demo", width=275)
+power_profile_file=st.file_uploader("Upload Power Profile file", type="csv")
+if power_profile_file is not None:
+    power_profile=pd.read_csv(power_profile_file, header=None)
+
+
+col1,col2,col3,col4,col5=st.columns(5)
+
+lens_front_rowIndex=col1.number_input("Lens Front Row Index", value=1)
+lens_back_rowIndex=col2.number_input("Lens Back Row Index", value=2)
+
+lens_diameter=col3.number_input("Lens Diameter", value=6.0)
+
+thickness_min=col4.number_input("Thickness Min", value=0.05)
+thickness_max=col5.number_input("Thickness Max", value=0.5)
+
+if_1st_differential=st.checkbox("Fit 1st Differential", value=True)
+
+col11,col12,col13=st.columns(3)
+thickness_weight=col11.number_input("Thickness Weight", value=1.0)
+power_weight=col12.number_input("Power Weight", value=1.0)
+if if_1st_differential:
+    differential_weight=col13.number_input("Differential Weight", value=1.0)
+
+build_button=st.button("Build Merit Function")
+MF_file=None
+if build_button and power_profile_file is not None:
+    MF_file=build_merit_function(
+        power_profile, 
+        lens_front_rowIndex,
+        lens_back_rowIndex,
+
+        lens_diameter,
+        thickness_min, 
+        thickness_max, 
+        
+        if_1st_differential)
+
+if MF_file is not None:
+    st.download_button("Dowload Merit Function", MF_file, file_name="MeritFunction.MF")
